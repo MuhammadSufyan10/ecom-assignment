d6f7700f782cf9c2bb3923b4b5658b7719c7417d
nabeelhussain456 <hnabeel570@gmail.com>
2024-12-09 00:51:15 +0500

    remove

---
+++ src/app/checkout/page.tsx
@@ -0,0 +1,11 @@
+/*************  ✨ Codeium Command ⭐  *************/
+/**
+ * Cart page.
+ *
+ * @returns Cart page element.
+ */
+/******  5ef26b75-8cf3-400f-be54-b0a7678703ca  *******/
+
+export default function Checkout() {
+  return <>Checkout</>;
+}
---
+++ src/app/shop/comparision.tsx
@@ -0,0 +1,3 @@
+export default function Comparision() {
+  return <>Comparision</>;
+}
